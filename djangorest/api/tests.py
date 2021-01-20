c85c3fa1417fe60f1f7103436f11d1d4fad35a93
Aaron Neff <aaron.neff@stackhawk.com>
2021-01-20 13:15:48 -0600

    added urls and additional test

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from .models import Bucketlist
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the bucketlist model."""
@@ -36,3 +36,32 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_bucketlist(self):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        """Test the api can get a given bucketlist."""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse('details',
+            kwargs={'pk': bucketlist.id}), format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_bucketlist(self):
+        """Test the api can update a given bucketlist."""
+        change_bucketlist = {'name': 'Something new'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            change_bucketlist, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_bucketlist(self):
+        """Test the api can delete a bucketlist."""
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('api.urls')) # Add this line
 ]
