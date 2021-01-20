3be7dd3f164bd71ed97b86edb5539aae313b95b2
Aaron Neff <aaron.neff@stackhawk.com>
2021-01-20 13:17:11 -0600

    get put delete view

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -14,3 +14,9 @@ class CreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
         serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
