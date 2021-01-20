2767a99c3ed056545f283ec0622879751b8cc21e
Aaron Neff <aaron.neff@stackhawk.com>
2021-01-20 12:58:43 -0600

    added view and view test

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -2,7 +2,9 @@
 # Create your tests here.
 from django.test import TestCase
 from .models import Bucketlist
-
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 class ModelTestCase(TestCase):
     """This class defines the test suite for the bucketlist model."""
@@ -18,3 +20,19 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
