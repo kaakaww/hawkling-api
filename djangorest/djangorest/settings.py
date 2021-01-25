23108e8d76deaf6c7753e81c68a2d02d6d58cf0b
Aaron <arneff@gmail.com>
2021-01-25 11:48:01 -0600

    Update settings.py

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w*i*+d+=7r81xj-hbu7plype-@b#16q@0$akdy*r2fb#%fn^!#'
+SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
