93f664deb4c807eb7ea5bea7eb67be0dbe0ede90
Aaron Neff <aaron.neff@stackhawk.com>
2021-01-25 11:53:32 -0600

    added os import for django key value

--- hawklingAPI/hawklingAPI/hawklingAPI/settings.py
+++ hawklingAPI/hawklingAPI/hawklingAPI/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tjcbj9lea)gfik^!54a+0ji)^dfk7$_vt$4-0$c+b=0%quxk%6'
-
+SECRET_KEY = os.getenv("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
